b43395b86b7ab90b99dfc8734b27be167b4b8e0f
r-rajaneesh <rajaneeshr2006@gmail.com>
2025-09-09 06:05:05 +0530

    🛠️ Basic Backend functionality of the system.

--- backend/index.ts
+++ backend/index.ts
@@ -1,12 +1,35 @@
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
-import { networkConnections, networkStats } from "systeminformation";
+import moment from "moment";
 const app = express();
-app.use(cors(), helmet());
-app.post("/attendance", (req, res) => {
-    const roll = req.body.rollno
-})
-app.listen(80, "0.0.0.0", () => {
-	console.log("Attendance Dashbaord running on http://127.0.0.1");
+const sql = new Bun.SQL("sqlite://attendance.db", {
+	prepare: false,
+});
+app.use(cors(), helmet(), express.json(), express.text());
+sql.connect();
+app.post("/attendance", async (req, res) => {
+	const body = req.body;
+	const roll = body.rollnumber;
+	const system = body.systemnumber ?? "Not specified";
+	const reason = body.reason ?? "Not specified";
+	console.log(roll, system, reason);
+	await sql`INSERT INTO attendance VALUES (${moment().unix()}, ${roll}, ${system}, ${reason})`;
+	res.json({ status: 200, body: { message: "Recieved Attendance" } });
+});
+app.listen(80, "0.0.0.0", async () => {
+	// Enable foreign keys
+	await sql`PRAGMA foreign_keys = ON`;
+
+	// Set journal mode to WAL for better concurrency
+	await sql`PRAGMA journal_mode = WAL`;
+	await sql`
+    CREATE TABLE IF NOT EXISTS attendance (
+        time TEXT NOT NULL,
+        rollno TEXT,
+        sysno TEXT,
+        reason TEXT
+    );
+    `;
+	console.log("Attendance Dashboard running on http://127.0.0.1");
 });
